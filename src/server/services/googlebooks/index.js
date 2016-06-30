e68f1f50a1d73cbee3cc876f6b8f969d128c332b
John <jomcode@gmail.com>
2016-06-30 16:26:02 -0500

    added googlebooks api endpoint
    fetches books based on params from google books api

---
+++ src/server/services/googlebooks/index.js
@@ -0,0 +1,47 @@
+'use strict';
+// const got = require('got');
+
+const hooks = require('./hooks');
+
+class Service {
+  constructor(options) {
+    this.options = options || {};
+  }
+
+  setup(app) {
+    this.app = app;
+  }
+
+  find(params) {
+    /* working - using mock data for development
+    const rootUrl = 'https://www.googleapis.com/books/v1/volumes';
+    const query = `q=${params.query.searchQuery}`;
+    const constraints = 'maxResults=40&orderBy=relevance&printType=books';
+    const fields = 'fields=items(id%2CselfLink%2CvolumeInfo(authors%2C' +
+      'categories%2Cdescription%2CimageLinks%2CmainCategory%2CpageCount%2C' +
+      'publishedDate%2Cpublisher%2Csubtitle%2Ctitle))';
+    const key = `key=${this.app.get('googleBooksKey')}`;
+
+    const target = `${rootUrl}?${query}&${constraints}&${fields}&${key}`;
+
+    return got(target)
+      .then(response => response.body)
+      .then(data => data)
+      .catch(error => console.error('googleBooks #find error', error));
+    */
+    return Promise.resolve(require('./mockdata.json'));
+  }
+}
+
+module.exports = function bootstrapGoogleBooks() {
+  const app = this;
+
+  app.use('/googlebooks', new Service());
+
+  const googleBooksService = app.service('/googlebooks');
+
+  googleBooksService.before(hooks.before);
+  googleBooksService.after(hooks.after);
+};
+
+module.exports.Service = Service;
--- src/server/services/index.js
+++ src/server/services/index.js
@@ -1,9 +1,11 @@
 const authentication = require('./authentication');
 const users = require('./user');
+const googleBooks = require('./googlebooks');
 
 module.exports = function bootstrapServices() {
   const app = this;
 
   app.configure(authentication);
   app.configure(users);
+  app.configure(googleBooks);
 };
