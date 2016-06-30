a6e6fbf2de56f6c22132f4b740dc9765099a4507
John <jomcode@gmail.com>
2016-06-30 13:45:56 -0500

    added ramda
    added ramda for compose and curry functions. specifically imported these functions in vendors entry point so that the whole library isn't included

--- src/client/vendor.js
+++ src/client/vendor.js
@@ -1,4 +1,9 @@
 import 'react';
 import 'react-dom';
 
+/* eslint-disable no-unused-vars */
+import { curry } from 'ramda/src/curry';
+import { compose } from 'ramda/src/compose';
+/* eslint-enable */
+
 import 'normalize.css';
