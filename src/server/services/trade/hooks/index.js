bb228e2f0107d1b7b6cd2c2e535180fa45b2b397
John <jomcode@gmail.com>
2016-07-08 16:15:58 -0500

    books are swapped between users on accepted trade

--- src/server/services/trade/hooks/index.js
+++ src/server/services/trade/hooks/index.js
@@ -11,12 +11,31 @@ exports.before = {
   remove: []
 };
 
+const swapBooks = hook => {
+  const bookService = hook.app.service('books');
+
+  const sentBy = hook.result.sentBy.slice();
+  const receivedBy = hook.result.receivedBy.slice();
+  const requestedBook = Object.assign({}, hook.result.requestedBook);
+  const proposedBook = Object.assign({}, hook.result.proposedBook);
+
+  const firstSwap = () =>
+    bookService.patch(requestedBook.bookId, { ownerId: sentBy });
+
+  const secondSwap = () =>
+    bookService.patch(proposedBook.bookId, { ownerId: receivedBy });
+
+  return Promise.all([firstSwap(), secondSwap()]).then(() => hook);
+};
+
 module.exports.after = {
   all: [],
   find: [],
   get: [],
   create: [],
   update: [],
-  patch: [],
+  patch: [
+    swapBooks
+  ],
   remove: []
 };
